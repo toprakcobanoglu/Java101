c2975e9c125b71910d2edf07fbe19d1ce87eb819
Toprak <114952372+toprakcobanoglu@users.noreply.github.com>
2022-12-25 18:53:49 +0300

    Add files via upload

---
+++ OBS/Teacher.java
@@ -0,0 +1,13 @@
+package java101.oop.OBS;
+
+public class Teacher {
+    String name;
+    String mpNo;
+    String branch;
+
+    public Teacher(String name, String mpNo, String branch)   {
+        this.name = name;
+        this.mpNo = mpNo;
+        this.branch = branch;
+    }
+}
